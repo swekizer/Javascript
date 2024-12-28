310630e419b497615e71e33a034018f252b6b7c5
swekizer <swekit7@outlook.com>
2024-12-29 00:37:33 +0530

    update

--- 01_basics/05_Strings.js
+++ 01_basics/05_Strings.js
@@ -9,3 +9,55 @@ let name2 = 'Doe';
 let text = "He's called 'John'";
 let text2 = 'He\'s called "John"';
 
+
+const namePerson = "John";
+const age = 30;
+
+// Concatenation
+console.log( "My name id" + namePerson + "my age is" + age);
+
+
+// Template literals
+console.log(`${namePerson} is ${age} years old`);
+
+
+const gameName = new String("PUBG");
+console.log(gameName);
+
+
+// String methods
+console.log("String methods");
+console.log(namePerson.length);
+console.log(namePerson.toUpperCase());
+console.log(namePerson.toLowerCase());
+console.log(namePerson[0]);
+console.log(namePerson.indexOf("J"));
+console.log(namePerson.lastIndexOf("n"));
+console.log(namePerson.charAt(2));
+console.log(namePerson.endsWith("n"));
+console.log(namePerson.includes("J"));
+console.log(namePerson.substring(0, 2));
+console.log(namePerson.slice(0, 2));
+console.log(namePerson.split(""));
+console.log(namePerson.replace("J", "K"));
+console.log(namePerson.trim());
+
+// Escape characters
+console.log("Hello\nWorld");
+console.log("Hello\tWorld");
+console.log("Hello\\World");
+console.log("Hello\"World");
+
+// String interpolation
+let a = 5;
+let b = 10;
+console.log(`Fifteen is ${a + b} and not ${2 * a + b}.`);
+
+// String concatenation
+let c = "Hello";
+let d = "World";
+console.log(c + " " + d);
+
+
+
+
