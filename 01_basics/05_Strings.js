ed20ca6e78b2d3ad7fa99243b86be1555b630bde
swekizer <swekit7@outlook.com>
2024-12-28 19:08:31 +0530

    new file

---
+++ 01_basics/05_Strings.js
@@ -0,0 +1,11 @@
+// Strings in javascript
+
+// Strings are used to store text. Strings must be enclosed in quotes. You can use single or double quotes:
+let name = "John";
+let name2 = 'Doe';
+
+// You can use quotes inside a string, as long as they don't match the quotes surrounding the string:
+
+let text = "He's called 'John'";
+let text2 = 'He\'s called "John"';
+
