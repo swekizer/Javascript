1a82aeb5ff9de6a23e0603aa3700359b46ca172e
swekizer <swekit7@outlook.com>
2025-01-01 18:07:00 +0530

    deleted

--- 02_basics/03_arrays.js
+++
@@ -1,5 +0,0 @@
-//Arrays
-
-//Arrays are a special type of object that stores a list of items. The items can be of any type, including other arrays. Arrays are created using square brackets []. Items in an array are separated by commas.
-
-const marvel_heros = ["Thor", "Ironman", "Spiderman"]
