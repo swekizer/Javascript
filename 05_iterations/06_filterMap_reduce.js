e5153afdf97d1aa3a5190a51eccab9acdd678031
swekizer <swekit7@outlook.com>
2025-05-16 17:10:36 +0530

    new

---
+++ 05_iterations/06_filterMap_reduce.js
@@ -0,0 +1,27 @@
+const coding = ["js", "ruby", "java", "python", "cpp"]
+
+const values = coding.forEach( (item) => {
+    console.log(item);
+    return item;
+})
+
+console.log(values);
+
+
+//filter
+const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+const newNums = myNums.filter( (num) => {
+    return num > 4
+})
+
+console.log(newNums);
+
+
+const newNums2 = []
+
+myNums.forEach( (num) => {
+    if (num > 4) {
+        newNums2.push(num)
+    }
+})
